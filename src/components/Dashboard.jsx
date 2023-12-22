cdbb0cb6a6d4adef1cc8188a20b98333eae83950
Ash151515 <roarhammer11@gmail.com>
2023-12-22 11:41:50 +0800

    Added fix for dashboard not displaying properly

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,10 +5,10 @@ import Home from "./Home";
 import SellerMenu from "./SellerMenu";
 import Suggestions from "./Suggestions";
 
-import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from './sample.png';
+import Carousel from "react-bootstrap/Carousel";
+import ExampleCarouselImage from "./sample.png";
 
-import '../App.css';
+import "../App.css";
 function Dashboard() {
   const clearSession = () => {
     UserProfile.clearSession();
@@ -18,7 +18,7 @@ function Dashboard() {
     const activeMenu = document.getElementsByClassName("active")[0];
     const menuToBeActivated = e.currentTarget;
     const home = document.getElementById("home");
-    const dashboardContent = document.getElementById("dashboardContent")
+
     const map = document.getElementById("googleMap");
     const sellerMenu = document.getElementById("sellerContent");
     const suggestions = document.getElementById("suggestionsContent");
@@ -27,7 +27,7 @@ function Dashboard() {
       menuToBeActivated.classList.add("active");
       console.log(menuToBeActivated.id);
       if (menuToBeActivated.id === "mainDashboard") {
-        dashboardContent.hidden = false;
+        home.hidden = false;
         map.hidden = true;
         sellerMenu.hidden = true;
         suggestions.hidden = true;
@@ -35,17 +35,17 @@ function Dashboard() {
         map.hidden = false;
         sellerMenu.hidden = true;
         suggestions.hidden = true;
-        dashboardContent.hidden = true;
+        home.hidden = true;
       } else if (menuToBeActivated.id === "sellerMenu") {
         sellerMenu.hidden = false;
         map.hidden = true;
         suggestions.hidden = true;
-        dashboardContent.hidden = true;
+        home.hidden = true;
       } else if (menuToBeActivated.id === "suggestionsMenu") {
         suggestions.hidden = false;
         map.hidden = true;
         sellerMenu.hidden = true;
-        dashboardContent.hidden = true;
+        home.hidden = true;
       }
     }
   };
@@ -210,62 +210,79 @@ function Dashboard() {
           </div>
         </nav>
       </header>
-      
-      <main style={{ marginTop: '55px' }}>
-            <div style={{ paddingLeft: '10vh' }} id="dashboardContent">
-              <div className="d-flex justify-content-between align-items-center" style={{ padding: '15px' }}>
-                <h1 style={{marginInlineStart: 100, padding: 15}}>Hello, [User Name]</h1>
-                <form className="input-group w-auto my-auto" style={{ display: 'flex', flexShrink: 0, minWidth: '300px' }}>
-                  <input
-                    autoComplete="off"
-                    type="search"
-                    className="form-control rounded"
-                    placeholder='What do you feel like eating today?'
-                    style={{minWidth: 300 + "px"}}
-                  />
-                  <span className="input-group-text border-0">
-                    <i className="fas fa-search"></i>
-                  </span>
-                </form>
-              </div>
 
-                <div>
-                  <h3>Take a look around USC-TC!</h3>
-                  <p>Learn about the go to grubs near the University of San Carlos Talamban Campus. Take a look at our website!</p>
-                  <Carousel>
-                    <Carousel.Item>
-                      <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                      </Carousel.Caption>
-                    </Carousel.Item>
-                  </Carousel>
-                </div>
+      <main style={{marginTop: "55px"}}>
+        <div style={{paddingLeft: "10vh"}} id="dashboardContent">
+          <div id="home">
+            <div
+              className="d-flex justify-content-between align-items-center"
+              style={{padding: "15px"}}
+            >
+              <h1 style={{marginInlineStart: 100, padding: 15}}>
+                Hello, {UserProfile.getName()}
+              </h1>
+              <form
+                className="input-group w-auto my-auto"
+                style={{display: "flex", flexShrink: 0, minWidth: "300px"}}
+              >
+                <input
+                  autoComplete="off"
+                  type="search"
+                  className="form-control rounded"
+                  placeholder="What do you feel like eating today?"
+                  style={{minWidth: 300 + "px"}}
+                />
+                <span className="input-group-text border-0">
+                  <i className="fas fa-search"></i>
+                </span>
+              </form>
+            </div>
 
-                <div style={{textAlign: "left", marginInlineStart: 100, padding: 15}}>
-                <div id="foodCategory">
-                  <h3>Category</h3>
-                </div>
+            <div>
+              <h3>Take a look around USC-TC!</h3>
+              <p>
+                Learn about the go to grubs near the University of San Carlos
+                Talamban Campus. Take a look at our website!
+              </p>
+              <Carousel>
+                <Carousel.Item>
+                  <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>
+                      Nulla vitae elit libero, a pharetra augue mollis interdum.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                      Praesent commodo cursus magna, vel scelerisque nisl
+                      consectetur.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </div>
+            <div id="foodCategory">
+              <h3>Category</h3>
+            </div>
 
-                <div id="recentOrders">
-                  <h3>Recent orders</h3>
-                </div>     
-                <div/>           
+            <div id="recentOrders">
+              <h3>Recent orders</h3>
+            </div>
+            <div />
+          </div>
 
+          <div style={{textAlign: "left", marginInlineStart: 100, padding: 15}}>
             <div id="googleMap" hidden>
               <GoogleMaps />
             </div>
@@ -278,23 +295,21 @@ function Dashboard() {
               <SellerMenu accountId={UserProfile.getAccountId()} />
             </div>
 
-                <div id="suggestionsContent" hidden>
-                  <Suggestions />
-                </div>
-
-              </div>
+            <div id="suggestionsContent" hidden>
+              <Suggestions />
             </div>
-          </main>
-
-          <div id="map" hidden>
-            <GoogleMaps />
           </div>
-
-          <div id="sellerContent" hidden>
-            <SellerMenu />
-          </div>
-
         </div>
+      </main>
+
+      <div id="map" hidden>
+        <GoogleMaps />
+      </div>
+
+      <div id="sellerContent" hidden>
+        <SellerMenu />
+      </div>
+    </div>
   ) : (
     <div>You do not have access to this page.</div>
   );
